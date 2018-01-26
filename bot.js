1a039d24dccb4da966cb33bdbd34b9445a5b55ed
Galboyan Eduard <galoboed@gmail.com>
2018-01-26 14:17:53 +0400

    Brought to working state

--- bot.js
+++ bot.js
@@ -1,24 +1,57 @@
 var TelegramBot = require('node-telegram-bot-api');
+var Queue = require('./queue');
 var token = require('./config').token;
+
+var maleQ = new Queue();
+var femaleQ = new Queue();
+var connections = [];
 
 var bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, function(msg, match) {
   var text = "What's your gender?";
- 
+
   var keyboardStr = JSON.stringify({
       inline_keyboard: [
         [
-          {text:'Male',callback_data:'You chosen Male.'},
-          {text:'Female',callback_data:'You chosen Female.'}
+          {text:'Male',callback_data:'_male'},
+          {text:'Female',callback_data:'_female'}
         ]
       ]
   });
- 
+
   var keyboard = {reply_markup: JSON.parse(keyboardStr)};
   bot.sendMessage(msg.chat.id, text, keyboard);
 });
 
 bot.on("callback_query", function(callbackQuery) {
-    bot.sendMessage(callbackQuery.message.chat.id, callbackQuery.data);
+    //bot.sendMessage(callbackQuery.message.chat.id, callbackQuery.data);
+    var isMale = (callbackQuery.data == '_male') ? true : false;
+
+    var oppositeQ = isMale ? femaleQ : maleQ;
+    var currentQ  = isMale ? maleQ : femaleQ;
+
+	if(oppositeQ.size() == 0){
+		currentQ.push(callbackQuery.message.chat.id);
+		bot.sendMessage(callbackQuery.message.chat.id,"Now there are no people we can connect you to. We'll inform you ASAP.");
+		return;
+	}
+
+	var currentChatId = (callbackQuery.message.chat.id).toString();
+	var partnerChatId = (oppositeQ.pop()).toString();
+
+	connections[currentChatId] = partnerChatId;		
+	connections[partnerChatId] = currentChatId;		
+	
+	var _message = "We've found a partner for you. Say hi to your parnter.";
+
+	bot.sendMessage(currentChatId, _message);
+	bot.sendMessage(partnerChatId, _message);
+});
+
+bot.on('message', function(msg, match) {
+	var currentChatId = (msg.chat.id).toString();
+	if(connections[currentChatId] != undefined){
+		bot.sendMessage(connections[currentChatId], msg.text);
+	};
 });
