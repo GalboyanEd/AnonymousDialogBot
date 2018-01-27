3401b43a4360c824907f9b799d1c3c89ded3e1da
Karen Martikyan <kamartikyan@gmail.com>
2018-01-27 19:01:24 +0400

    Fixed many bugs + changed queue type + remember gender

--- bot.js
+++ bot.js
@@ -1,53 +1,101 @@
 var TelegramBot = require('node-telegram-bot-api');
-var Queue = require('./queue');
 var token = require('./config').token;
 
-var maleQ = new Queue();
-var femaleQ = new Queue();
-var connections = [];
+var maleQ = [];
+var femaleQ = [];
+var connections = {};
+var genders = {};
 
 var bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, function(msg, match) {
-  var text = "What's your gender?";
+	if(genders[msg.chat.id] != undefined) {
+		findPartner(msg.chat.id);
+		return;
+	}
 
-  var keyboardStr = JSON.stringify({
-      inline_keyboard: [
-        [
-          {text:'Male',callback_data:'_male'},
-          {text:'Female',callback_data:'_female'}
-        ]
-      ]
-  });
+	var text = "What's your gender?";
 
-  var keyboard = {reply_markup: JSON.parse(keyboardStr)};
-  bot.sendMessage(msg.chat.id, text, keyboard);
+	var keyboardStr = JSON.stringify({
+			inline_keyboard: [
+				[
+					{text:'Male',callback_data:'_male'},
+					{text:'Female',callback_data:'_female'}
+				]
+			]
+	});
+
+	var keyboard = {reply_markup: JSON.parse(keyboardStr)};
+	bot.sendMessage(msg.chat.id, text, keyboard);
 });
 
 bot.on("callback_query", function(callbackQuery) {
-    var isMale = (callbackQuery.data == '_male') ? true : false;
+		var isMale = (callbackQuery.data == '_male') ? true : false;
+		genders[callbackQuery.message.chat.id] = isMale;
+	findPartner(callbackQuery.message.chat.id);
+})
 
-    var oppositeQ = isMale ? femaleQ : maleQ;
-    var currentQ  = isMale ? maleQ : femaleQ;
-
-    var _waitMessage = "Now there are no people we can connect you to. We'll inform you ASAP.";
-
-	if(oppositeQ.size() == 0){
-		currentQ.push(callbackQuery.message.chat.id);
-		bot.sendMessage(callbackQuery.message.chat.id,_waitMessage);
+bot.on('message', function(msg, match) {
+	if((msg.text).indexOf('/log') == 0){
+	console.log('msg.chat.id is ' + msg.chat.id);
+	console.log('femaleQ is ' + femaleQ);
+	console.log('makeQ is ' + maleQ);
+	console.log('connections is ' + JSON.stringify(connections));
+	console.log('genders is ' + JSON.stringify(genders));
 		return;
 	}
 
-	var currentChatId = (callbackQuery.message.chat.id).toString();
-	var partnerChatId = (oppositeQ.pop()).toString();
+	var currentChatId = msg.chat.id;
+	if(connections[currentChatId] != undefined){
+		bot.sendMessage(connections[currentChatId], msg.text);
+	};
+});	
+
+
+bot.onText(/\/changePartner/, function(msg, match) {
+	var currentChatId = msg.chat.id;
+	var partnerChatId = connections[currentChatId];
+
+	connections[currentChatId] = undefined;
+	connections[partnerChatId] = undefined;
+
+	bot.sendMessage(currentChatId, "We've blocked your partner.");
+	bot.sendMessage(partnerChatId, "Your partner blocked you.");
+
+	var _message = 'Type /start to find a partner.';
+
+	bot.sendMessage(currentChatId, _message);
+	bot.sendMessage(partnerChatId, _message);
+})
+
+function isInQueue(chatID, queue){
+	return !(queue.indexOf(chatID) == -1);
+}
+
+function findPartner(chatID){
+	isMale = genders[chatID];
+
+		var oppositeQ = isMale ? femaleQ : maleQ;
+		var currentQ	= isMale ? maleQ : femaleQ;
+
+		var _waitMessage = "Now there are no people we can connect you to. We'll inform you ASAP.";
+
+	if(oppositeQ.length == 0){
+		currentQ.push(chatID);
+		bot.sendMessage(chatID,_waitMessage);
+		return;
+	}
+
+	var currentChatId = chatID;
+	var partnerChatId = oppositeQ.shift();
 
 	if(currentChatId == partnerChatId){
-		bot.sendMessage(callbackQuery.message.chat.id,_waitMessage);
+		bot.sendMessage(chatID,_waitMessage);
 		return;
 	}
 
-	connections[currentChatId] = {"partner": partnerChatId, "isMale": !isMale};		
-	connections[partnerChatId] = {"partner": currentChatId, "isMale": isMale};		
+	connections[currentChatId] = partnerChatId;		
+	connections[partnerChatId] = currentChatId;		
 	
 	var _message = "We've found someone for you. Say hi to your parnter.";
 
@@ -55,72 +103,21 @@ bot.on("callback_query", function(callbackQuery) {
 	bot.sendMessage(partnerChatId, _message);
 
 	var option = {
-        "parse_mode": "Markdown",
-        "reply_markup": {
-            "one_time_keyboard": true,
-            "keyboard": [[{
-                text: "/changePartner"
-            }]]
-        }
-    };
+				"parse_mode": "Markdown",
+				"reply_markup": {
+						"one_time_keyboard": true,
+						"keyboard": [[{
+								text: "/changePartner"
+						}]]
+				}
+		};
 
-    _message = "You can always change your partner by typing /changePartner";
+		_message = "You can always change your partner by typing /changePartner";
 
 	bot.sendMessage(currentChatId, _message, option);
 	bot.sendMessage(partnerChatId, _message, option);
+}
+
+bot.on('polling_error', function(error){
+	console.log(error);
 });
-
-bot.on('message', function(msg, match) {
-	if((msg.text).indexOf('/') == 0){
-		return;
-	}
-
-	var currentChatId = (msg.chat.id).toString();
-	if(connections[currentChatId] != undefined){
-		bot.sendMessage(connections[currentChatId].partner, msg.text);
-	};
-});	
-
-
-bot.onText(/\/changePartner/, function(msg, match) {
-	var currentChatId = (msg.chat.id).toString();
-
-	var partnerChat = connections[currentChatId];
-	var currentChat = connections[partnerChat.partner];
-
-	var partnerChatId = partnerChat.partner;
-
-	connections[currentChatId] = undefined;
-	connections[partnerChatId] = undefined;
-
-	if(currentChat.isMale == true)	{
-		maleQ.push(currentChatId);
-		femaleQ.push(partnerChatId);
-	} else {
-		maleQ.push(partnerChatId);
-		femaleQ.push(currentChatId);
-	}
-
-	bot.sendMessage(currentChatId, "We've blocked your partner.");
-	bot.sendMessage(partnerChatId, "Your partner blocked you.");
-
-	var partnerCall = {};
-	if(partnerChat.isMale){
-		partnerCall.data = '_male';
-	} else {
-		partnerCall.data = '_female'
-	}
-
-	var currentCall = {};
-	if(currentChat.isMale){
-		currentCall.data = '_male';
-	} else {
-		currentCall.data = '_female'
-	}
-
-	currentCall.message = {"chat" : {"id" : currentChatId}};
-	partnerCall.message = {"chat" : {"id" : partnerChatId}};
-
-	setTimeout(bot.emit, 1000, 'callback_query', currentCall);
-	setTimeout(bot.emit, 2000, 'callback_query', partnerCall);
-})
