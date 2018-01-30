4723329dd312885b41d921903fe32c4999a2d094
Karen Martikyan <kamartikyan@gmail.com>
2018-01-30 12:04:54 +0400

    Minor changes

--- js/bot.js
+++ js/bot.js
@@ -120,10 +120,9 @@ function findPartner(chatID){
 				"parse_mode": "Markdown",
 				"reply_markup": {
 						"one_time_keyboard": true,
-						"keyboard": [[{
-								text: "/endChat"
-						},
-                            {text: "/start"}
+						"keyboard": [[
+							{text: "/start"},
+							{text: "/endChat"}
                         ]]
 				}
 		};
